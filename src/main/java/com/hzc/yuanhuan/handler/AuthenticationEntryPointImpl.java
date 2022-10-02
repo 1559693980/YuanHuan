b9632a5ab438fe395035f7467f7bf1515e5e6988
hzc <2579717671@qq.com>
2022-10-02 12:50:55 +0800

    --

--- src/main/java/com/hzc/yuanhuan/handler/AuthenticationEntryPointImpl.java
+++ src/main/java/com/hzc/yuanhuan/handler/AuthenticationEntryPointImpl.java
@@ -1,10 +1,10 @@
-package com.hzc.yuanhuan.hander;
+package com.hzc.yuanhuan.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hzc.yuanhuan.result.ComResult;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
@@ -12,15 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
 @Component
-public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        ComResult<String> result = new ComResult<>(HttpStatus.FORBIDDEN.value(), "您的权限不足", null);
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ComResult<String> result = new ComResult<>(HttpStatus.UNAUTHORIZED.value(), "用户认证失败请重新登陆", null);
         response.setContentType("application/json;charset=utf-8");
         PrintWriter writer = response.getWriter();
         writer.write(objectMapper.writeValueAsString(result));
